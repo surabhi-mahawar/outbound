f5320b14aaac672baadab7a01efc8e5eb642551c
Vishal Singla <vishal.singla@oyorooms.com>
2020-05-15 10:44:31 +0530

    kafka integrate

--- src/main/java/com/samagra/Application/GupshupConsumer.java
+++ src/main/java/com/samagra/Application/GupshupConsumer.java
@@ -1,12 +1,9 @@
 package com.samagra.Application;
 
-import java.util.HashMap;
-import javax.xml.bind.JAXBException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -24,24 +21,24 @@ public class GupshupConsumer {
 
   @Autowired
   private MS3Service ms3Service;
+  //
+  // @RequestMapping(value = "/post", method = RequestMethod.POST)
+  // public void main(String arg[])
+  // throws Exception {
+  // String message = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
+  // + "<messageResponse>\n" + " <app>DemoApp</app>\n" + " <payload>\n"
+  // + " <id>ABEGkYaYVSEEAhAL3SLAWwHKeKrt6s3FKB0c</id>\n" + " <payload>\n"
+  // + " <caption>hahah</caption>\n" + " <text>Hi</text>\n"
+  // + " <url>url</url>\n" + " </payload>\n" + " <sender>\n"
+  // + " <name>Smit</name>\n" + " <phone>918x98xx21x4</phone>\n"
+  // + " </sender>\n" + " <type>optedOut</type>\n" + " </payload>\n"
+  // + " <timestamp>1580227766370</timestamp>\n" + " <type>userevent</type>\n"
+  // + " <version>2</version>\n" + "</messageResponse>\n" + "\n" + "";
+  //
+  // consumeMessage(message);
+  // }
 
-  @RequestMapping(value = "/post", method = RequestMethod.POST)
-  public void main(String arg[])
-      throws Exception {
-    String message = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n"
-        + "<messageResponse>\n" + "    <app>DemoApp</app>\n" + "    <payload>\n"
-        + "        <id>ABEGkYaYVSEEAhAL3SLAWwHKeKrt6s3FKB0c</id>\n" + "        <payload>\n"
-        + "            <caption>hahah</caption>\n" + "            <text>Hi</text>\n"
-        + "            <url>url</url>\n" + "        </payload>\n" + "        <sender>\n"
-        + "            <name>Smit</name>\n" + "            <phone>918x98xx21x4</phone>\n"
-        + "        </sender>\n" + "        <type>optedOut</type>\n" + "    </payload>\n"
-        + "    <timestamp>1580227766370</timestamp>\n" + "    <type>userevent</type>\n"
-        + "    <version>2</version>\n" + "</messageResponse>\n" + "\n" + "";
-
-    consumeMessage(message);
-  }
-
-  // @KafkaListener(id = "message", topics = "gs-incoming-message")
+   @KafkaListener(id = "message", topics = "gs-incoming-message")
   public void consumeMessage(String message)
       throws Exception {
 
@@ -51,7 +48,7 @@ public class GupshupConsumer {
     ms3Service.processKafkaInResponse(value);
   }
 
-  // @KafkaListener(id = "userevent", topics = "gs-opted-out-message")
+   @KafkaListener(id = "userevent", topics = "gs-opted-out-message")
   public void consumeOptedOutMessage(String message)
       throws JsonMappingException, JsonProcessingException {
     XmlMapper xmlMapper = new XmlMapper();
