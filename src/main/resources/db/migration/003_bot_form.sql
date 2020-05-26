e0f3457bf2a77b421a10baab72034561ac7663ff
Vishal Singla <vishal.singla@oyorooms.com>
2020-05-26 20:07:44 +0530

    Revert "db fix"
    This reverts commit 9572a628e6173f006156594653c87084d22e42d9.

--- src/main/resources/db/migration/003_bot_form.sql
+++ src/main/resources/db/migration/003_bot_form.sql
@@ -1,9 +1,9 @@
 create table bot_form(
 	id BIGSERIAL PRIMARY KEY NOT NULL,
-	form_id BIGSERIAL NOT NULL,
+	form_id BIGSERIAL PRIMARY KEY NOT NULL,
 	name VARCHAR(15) NOT NULL,
 	welcome_message text ,	
-	wrong_default_message VARCHAR(100)
+	wrong_default_message VARCHAR(100);	
 );
 
 
