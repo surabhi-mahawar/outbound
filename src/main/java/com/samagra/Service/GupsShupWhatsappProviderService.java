12d2d2b961950d0188e18b445d40027d4f1671ce
chaks03 <chakshu@samagragovernance.in>
2020-05-28 22:29:12 +0530

    Fix: ODK form submission

--- src/main/java/com/samagra/Service/GupsShupWhatsappProviderService.java
+++ src/main/java/com/samagra/Service/GupsShupWhatsappProviderService.java
@@ -1,22 +1,44 @@
 package com.samagra.Service;
 
+import java.io.*;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.nio.file.Files;
+import java.util.*;
 import javax.xml.bind.JAXBException;
+
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpHost;
 import org.apache.http.NameValuePair;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.HttpClient;
 import org.apache.http.client.utils.URLEncodedUtils;
+import org.apache.http.impl.client.*;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.tomcat.util.http.fileupload.FileItem;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.apache.tomcat.util.http.fileupload.disk.DiskFileItem;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -29,7 +51,11 @@ import com.samagra.Repository.StateRepository;
 import com.samagra.common.Request.Message;
 import com.samagra.notification.Response.MS3Response;
 import com.samagra.notification.Response.MessageResponse;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.mock.web.MockMultipartFile;
 
+@Slf4j
 @Service
 public class GupsShupWhatsappProviderService extends AbstractProvider implements IProvider {
 
@@ -37,14 +63,52 @@ public class GupsShupWhatsappProviderService extends AbstractProvider implements
   private String gupshupWhatsappApp;
   private final static String GUPSHUP_OUTBOUND = "https://api.gupshup.io/sm/api/v1/msg";
 
+  @Qualifier("rest")
   @Autowired
-  private static RestTemplate restTemplate;
+  private RestTemplate restTemplate;
+
+  @Qualifier("custom")
+  @Autowired
+  private RestTemplate customRestTemplate;
 
   @Autowired
   private StateRepository stateRepo;
 
   @Autowired
   private MessageRepository msgRepo;
+
+  private CredentialsProvider provider() {
+    CredentialsProvider provider = new BasicCredentialsProvider();
+    UsernamePasswordCredentials credentials = new UsernamePasswordCredentials("samagra", "impact@scale");
+    provider.setCredentials(AuthScope.ANY, credentials);
+    return provider;
+  }
+
+  private static HttpHeaders getVerifyHttpHeader4() throws Exception {
+    HttpHeaders headers = new HttpHeaders();
+    headers.setContentType(MediaType.APPLICATION_JSON);
+    return headers;
+  }
+
+  class MultipartInputStreamFileResource extends InputStreamResource {
+
+    private final String filename;
+
+    MultipartInputStreamFileResource(InputStream inputStream, String filename) {
+      super(inputStream);
+      this.filename = filename;
+    }
+
+    @Override
+    public String getFilename() {
+      return this.filename;
+    }
+
+    @Override
+    public long contentLength() throws IOException {
+      return -1; // we do not want to generally read the whole stream into memory ...
+    }
+  }
 
   @Override
   public void processInBoundMessage(MS3Response ms3Response, MessageResponse kafkaResponse)
@@ -54,13 +118,52 @@ public class GupsShupWhatsappProviderService extends AbstractProvider implements
     replaceUserState(ms3Response, kafkaResponse);
     appendNewResponse(ms3Response, kafkaResponse);
 
-    boolean isLastResponse = ms3Response.getCurrentIndex() == null ? true : false;
+    boolean isLastResponse = ms3Response.getCurrentIndex().equals("endOfForm") ? true : false;
 
     if (isLastResponse) {
       // call to odk
+      String ODK = "http://aggregate.cttsamagra.xyz:8080/Aggregate.html#submissions/filter///";
+      String ODK2 = "http://aggregate.cttsamagra.xyz:8080/submission";
+      HttpEntity<String> request = new HttpEntity<String>(getVerifyHttpHeader4());
+      HttpEntity<String> response = customRestTemplate.exchange(ODK, HttpMethod.GET, request, String.class);
+      System.out.println(new ObjectMapper().writeValueAsString(response));
+      HttpHeaders headers = response.getHeaders();
+      String set_cookie = headers.getFirst(HttpHeaders.SET_COOKIE);
+      log.info("set-cookie {}", set_cookie);
+
+      String fileName = "instance-"+  new Date().getTime() + ".xml";
+      File f = new File(fileName);
+      FileWriter myWriter = new FileWriter(fileName);
+      myWriter.write(ms3Response.getCurrentResponseState());
+      myWriter.close();
+      File myObj = new File(fileName);
+      MultiValueMap<String, Object> body = new LinkedMultiValueMap<>();
+
+      // Took 3 hours to just resolve this.
+      body.add("xml_submission_file", new FileSystemResource(myObj));
+
+      HttpEntity<MultiValueMap<String, Object>> request2 = new HttpEntity<>(body, getVerifyHttpHeader2(set_cookie));
+      restTemplate.getMessageConverters().add(getMappingJackson2HttpMessageConverter());
+      HttpEntity<String> response2 = customRestTemplate.postForEntity(ODK2, request2, String.class);
+      myObj.delete();
+      log.info("response2 {}", response2);
+
     } else {
       sendGupshupWhatsAppOutBound(ms3Response, kafkaResponse);
     }
+  }
+
+  private static HttpHeaders getVerifyHttpHeader3() throws Exception {
+    HttpHeaders headers = new HttpHeaders();
+    return headers;
+  }
+
+  private static HttpHeaders getVerifyHttpHeader2(String str) throws Exception {
+    HttpHeaders headers = new HttpHeaders();
+    headers.add("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
+    headers.add("content-type", "multipart/form-data; boundary=----WebKitFormBoundarydaqjAWcH4XdvwPoz");
+    headers.add("cookie", str);
+    return headers;
   }
 
 
